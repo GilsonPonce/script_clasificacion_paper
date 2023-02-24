eb362d1c276aaa9af2dfc89cff4fca32a4c828f4
gponce <gponce@comextweb.com>
2023-02-24 17:09:32 -0500

    correccion de seleccion del doi

--- test2.py
+++ test2.py
@@ -94,7 +94,7 @@ def buscar_paper_sin_doi(list_archivos_bib):
                 info_para_excel.append(namearch)
             if linea.startswith('doi'):
                 have_doi = True
-                info_para_excel.append(linea[4:-2])
+                info_para_excel.append(linea[5:-2])
                 info_para_excel.reverse()
                 list_doi.append(tuple(info_para_excel))
             if linea.startswith('@'):  # nuevo articulo
